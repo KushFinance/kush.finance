dcab9b46acf67582d181d04fca47d56ff6d216c6
Kush Smoker 420 <70850757+KushFinance@users.noreply.github.com>
2020-09-24 23:25:58 +0300

    Update GovernPage.jsx

--- client/src/views/GovernPage.jsx
+++ client/src/views/GovernPage.jsx
@@ -4,16 +4,14 @@ export default class Pump extends Component {
   render() {
     return (
       <div className="subpage govern">
-        <h1>PUMP FUND</h1>
-        <h2>Welcome to the steering wheel of a multi-million dollar machine</h2>
-        <Divider />
+        <h1>Governing Page & Fund FAQ</h1>
+        <Divider orientation="left">Welcome to the steering wheel of a multi-million dollar machine</Divider>
         <p>In this system, kSEED holders will be able to freely govern the protocol as they wish. </p>
         <p>The voting and function calls operate on a bidding system.</p>
         <p>In which the bid with the most votes at the end of a round will be executed.</p>
         <p>A bid can be a call to a contract, or a combination of calls to one or multiple contracts.</p>
         <p>All within the same transaction.</p>
-        <h2>What are Bids?</h2>
-        <Divider />
+        <Divider orientation="left">What are Bids?</Divider>
         <p> Glad you asked! Bids are a set of instructions that relay what functions to call with the Voting Connector contract. </p>
         <p> A bid is composed of the Bidder's address, a unique bidId, and a series of arrays with types including Integers, Strings, and Bytes. </p>
         <p> With these data fields, anyone will be able to construct a bid with its own set of parameters for the function to be called.</p>
