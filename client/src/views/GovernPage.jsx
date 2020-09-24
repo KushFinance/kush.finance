96e5a79c69665ec5d2d417fbfea9368c2b529844
Kush Smoker 420 <70850757+KushFinance@users.noreply.github.com>
2020-09-24 05:13:03 +0300

    updates

--- client/src/views/GovernPage.jsx
+++ client/src/views/GovernPage.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import { Divider } from "antd";
-
+import Balance from "./components/Balance";
 export default class Pump extends Component {
   render() {
     return (
       <div className="subpage govern">
         <h1>PUMP FUND</h1>
-        <h2>Let's build a whale!</h2>
+        <h2>Let's build a mini-whale or a mega-whale?!</h2>
         <Divider />
-        <p>kSEED is introducing a publically governed Uniswap Hedge fund.</p>
+        <p>KUSH.FINANCE is introducing a publically governed Uniswap Hedge fund.</p>
         <p> 20% of all minted KUSH goes to a funding contract every few days.</p>
         <p> kSEED holders will be able to vote on which Uniswap token the contract will swap for.</p>
-        <p> The contract will hold the token for a specified amount of blocks or until kSEED holders vote for a swap back to KUSH</p>
-        <p> During a swap back to KUSH, the resulting KUSH will be proportionately distributed to kSEED holders.</p>
+        <p> The contract will hold the token for a specified amount of blocks or until kSEED holders vote for a swap back to kKUSH</p>
+        <p> During a swap back to kKUSH, the resulting kKUSH will be proportionately distributed to kSEED holders.</p>
         <button className="disabled launch-date">Voting starts late October</button>
       </div>
     );
