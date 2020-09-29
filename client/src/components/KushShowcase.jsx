33c3c8a28bb22c436ae4aa7513613630a7dcc424
Kush Smoker 420 <70850757+KushFinance@users.noreply.github.com>
2020-09-29 15:27:43 +0300

    Uniswap Test

--- client/src/components/KushShowcase.jsx
+++ client/src/components/KushShowcase.jsx
@@ -16,6 +16,20 @@ class KushShowcase extends Component {
                  <Button href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x538b4b507d57bf9ebd8847ec395b7b061c150181" target="_blank" rel="noopener noreferrer" type="primary" size="middle">BUY kKUSH</Button>
                  <Button href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x8DdF8Af6A26D316Ac07269dd490BBFb31718A3d4" target="_blank" rel="noopener noreferrer" type="primary" size="middle">BUY kOG</Button>
                  </Space>
+                 <iframe
+  src="https://app.uniswap.org/#/swap?outputCurrency=0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359"
+  height="660px"
+  width="100%"
+  style="
+    border: 0;
+    margin: 0 auto;
+    display: block;
+    border-radius: 10px;
+    max-width: 600px;
+    min-width: 300px;
+  "
+  id="myId"
+/>
                     <ul className="addresses">
                      <li>
                         <a href="https://etherscan.io/address/0x3f09400313e83d53366147e3ea0e4e2279d80850" target="_blank" rel="noopener noreferrer">
