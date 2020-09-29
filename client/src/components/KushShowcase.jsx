1f191f19a4c4238ab12c620de3aa3307fbc7a5f9
Kush Smoker 420 <70850757+KushFinance@users.noreply.github.com>
2020-09-29 16:01:02 +0300

    Showcase

--- client/src/components/KushShowcase.jsx
+++ client/src/components/KushShowcase.jsx
@@ -17,12 +17,16 @@ class KushShowcase extends Component {
                  <Button href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x538b4b507d57bf9ebd8847ec395b7b061c150181" target="_blank" rel="noopener noreferrer" type="primary" size="middle">BUY kKUSH</Button>
                  <Button href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x8DdF8Af6A26D316Ac07269dd490BBFb31718A3d4" target="_blank" rel="noopener noreferrer" type="primary" size="middle">BUY kOG</Button>
                  </Space>
+                 <List
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={item => (
                  <List.Item>
         <List.Item.Meta
           avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
           title={<a href="https://ant.design">{item.title}</a>}
           description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        /></List.Item>
+        /></List.Item>  )}/>
                     <ul className="addresses">
                      <li>
                         <a href="https://etherscan.io/address/0x3f09400313e83d53366147e3ea0e4e2279d80850" target="_blank" rel="noopener noreferrer">
