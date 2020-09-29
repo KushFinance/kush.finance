580182d3e8f4c1548fcce980e361bedc58a17cd3
Kush Smoker 420 <70850757+KushFinance@users.noreply.github.com>
2020-09-29 17:51:49 +0300

    NFT

--- client/src/views/KushARTPage.jsx
+++ client/src/views/KushARTPage.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { Tabs } from "antd";
+import { Tabs, Image } from "antd";
+import NFT from '../assets/elon-musk-demo-nft.png'
 const { TabPane } = Tabs;
 export default class KushARTPage extends Component {
   render() {
     return (
       <Tabs defaultActiveKey="1">
       <TabPane tab="INFO" key="1">
-
+      <Image preview={false} width={450} src={NFT}/>
           <p>More technical details about how KushART project will work soon....</p>
        
         <button className="disabled launch-date">KushART Opens For Public in Late Autumn</button>
