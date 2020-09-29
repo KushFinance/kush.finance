c48f5bf99e4ef818485ac044eb7d1dcd24bb6867
Kush Smoker 420 <70850757+KushFinance@users.noreply.github.com>
2020-09-29 15:47:37 +0300

    Update KushShowcase.jsx

--- client/src/components/KushShowcase.jsx
+++ client/src/components/KushShowcase.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Logo from '../assets/logo.png'
 import Footer from './Footer';
-import {Image, Button, Card, Typography, Space  } from "antd";
+import { List, Avatar, Image, Button, Card, Typography, Space  } from "antd";
 const { Paragraph } = Typography;
 class KushShowcase extends Component {
     render(){
@@ -16,7 +16,17 @@ class KushShowcase extends Component {
                  <Button href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x538b4b507d57bf9ebd8847ec395b7b061c150181" target="_blank" rel="noopener noreferrer" type="primary" size="middle">BUY kKUSH</Button>
                  <Button href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x8DdF8Af6A26D316Ac07269dd490BBFb31718A3d4" target="_blank" rel="noopener noreferrer" type="primary" size="middle">BUY kOG</Button>
                  </Space>
-                 
+                  <List
+                    itemLayout="horizontal"
+                     dataSource={data}
+                     renderItem={item => (
+                   <List.Item>
+                    <List.Item.Meta
+                       avatar={<Avatar src="https://etherscan.io/images/brandassets/etherscan-logo-circle.png" />}
+                         title={<a href="https://etherscan.io/address/0x3f09400313e83d53366147e3ea0e4e2279d80850">kSEED Contract address</a>}
+                         description="kSEED Etherscan Official Contract"
+                       /><Paragraph copyable> 0x3f09400313e83d53366147e3ea0e4e2279d80850</Paragraph>
+                  </List.Item>,
                     <ul className="addresses">
                      <li>
                         <a href="https://etherscan.io/address/0x3f09400313e83d53366147e3ea0e4e2279d80850" target="_blank" rel="noopener noreferrer">
